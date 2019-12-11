2c6fb517c3334a5d2e0c8090d9d0efe6ac04cf25
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-12-11 10:38:17 +0100

    korrigierte Version

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -1,24 +1,31 @@
 import java.sql.*;
 
 public class Benchmark {
-
-	static String url = "192.168.122.70";
-	static String database = "dbi_benchmark";
-	static String user = "root";
-	static String pw = "root";
 	static Statement stmnt;
+	static DBConnector connector;
+	private static String url;
+	private static String database;
+	private static String user;
+	private static String pw;
+	static DB_Daten daten ;
 	
 	public static void main(String[] args) throws SQLException {
+		//initialisiere Daten
+		daten = new DB_Daten();
+		url = daten.getUrl();
+		database = daten.getDatabase();
+		user = daten.getUser();
+		pw = daten.getPw();
+		
+		
 		//Starte run
 		double t1 = System.currentTimeMillis();
 
-				
 		//connect to DB
-
 		DBConnector connector = new DBConnector(); 
-		connector.connectToDB(url, database, user, pw);
-		stmnt = connector.connection.createStatement();
+		connector.connectToDB(url,database,user,pw);
 		System.out.println("verbunden: "+ (System.currentTimeMillis()-t1)+" ms");
+		stmnt = connector.connection.createStatement();
 
 	}
 
