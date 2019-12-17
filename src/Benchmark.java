fb21815c0b1e53fac899501e3b6df15d700a015b
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-12-17 20:23:01 +0100

    1.0

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -3,30 +3,43 @@ import java.sql.*;
 public class Benchmark {
 	static Statement stmnt;
 	static DBConnector connector;
-	private static String url;
-	private static String database;
-	private static String user;
-	private static String pw;
+
 	static DB_Daten daten ;
 	
 	public static void main(String[] args) throws SQLException {
 		//initialisiere Daten
 		daten = new DB_Daten();
-		url = daten.getUrl();
-		database = daten.getDatabase();
-		user = daten.getUser();
-		pw = daten.getPw();
+
 		
 		
 		//Starte run
 		double t1 = System.currentTimeMillis();
 
 		//connect to DB
-		DBConnector connector = new DBConnector(); 
-		connector.connectToDB(url,database,user,pw);
-		System.out.println("verbunden: "+ (System.currentTimeMillis()-t1)+" ms");
-		stmnt = connector.getConnection().createStatement();
-
+		connector = new DBConnector(); 
+		connector.connectToDB(daten.getUrl(),daten.getDatabase(),daten.getUser(),daten.getPw());
+		System.out.println("verbunden: "+vergangeneZeit(t1) +" ms");
+		
+		
+		if(vergangeneZeit(t1)>= 10)
+		{
+			System.out.println("Beginne mit Transaktionen");
+			for(int i =0; i<6;i++)
+			{
+				Loaddriver ld = new Loaddriver();
+				ld.start();
+			}
+		}
+		if(vergangeneZeit(t1)==600000)
+		{
+			System.exit(0);
+		}
+		
 	}
 
+	public static double vergangeneZeit(double startTime)
+	{
+		return System.currentTimeMillis()-startTime;
+	}
+	
 }
--- src/Loaddriver.java
+++ src/Loaddriver.java
@@ -1,59 +1,140 @@
 import java.sql.*;
-public class Loaddriver {
+public class Loaddriver extends Thread {
 	static DBConnector connector;
-	private static String url;
-	private static String database;
-	private static String user;
-	private static String pw;
 	static DB_Daten daten ;
 	private static boolean running;
+	Statement stmnt ;
+	private int txCount;
+	double t1;
 	
+	public Loaddriver() throws SQLException
+	{
+		daten = new DB_Daten();
+		connector = new DBConnector(); 
+		connector.connectToDB(daten.getUrl(),daten.getDatabase(),daten.getUser(),daten.getPw());
+		System.out.println("verbunden");
+		stmnt = connector.getConnection().createStatement();
+	}
 	
 	public boolean isRunning() {
 		return running;
 	}
 
-
 	public void setRunning(boolean run) {
 		running = run;
 	}
 
-
-	public static void main(String[] args) throws SQLException, InterruptedException {
-		
-		daten = new DB_Daten();
-		url = daten.getUrl();
-		database = daten.getDatabase();
-		user = daten.getUser();
-		pw = daten.getPw();
-		DBConnector connector = new DBConnector(); 
-		connector.connectToDB(url,database,user,pw);
-		PreparedStatement pStmntBalance = connector.getConnection().prepareStatement("Select balance from accounts where accid=?");
+	public void run()
+	{
+		t1 = System.currentTimeMillis();
+		this.setRunning(true);
+		txCount = 0;
+			try {
+				transactions();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	public void transactions() throws SQLException, InterruptedException {	
 		while(running)
 		{
-			
+			//Zufallszahl zur Bestimmung der Transaktion
 			int zZahl = (int)(Math.random()*100+1);
 			if(zZahl <36)
 			{
-				int zAccId =(int)(Math.random()*10000000+1);
-				pStmntBalance.setInt(1, zAccId);
-				ResultSet rs = pStmntBalance.executeQuery();
-				while(rs.next())
-				{
-					System.out.println(rs.getString(1));
-				}
+				this.txBalance();
+				txCount++;
 			}
 			else if(zZahl<86)
 			{
-				
+				this.txDeposit();
+				txCount++;
 			}
 			else
 			{
-				
+				this.txAnalysis();
+				txCount++;
 			}
 			Thread.sleep(50);
+			if(System.currentTimeMillis()-t1 == 300000)
+			{
+				this.setRunning(false);
+				System.out.println(txCount +" Transaktionen");
+				System.out.println(txCount / 300+" Transaktionen pro Sekunde");
+			}
 		}
 		
 	}
 
+	public double txBalance() throws SQLException
+	{
+		ResultSet rs = null;
+		int accid =(int)(Math.random()*10000000+1) ;
+		try {
+			rs = stmnt.executeQuery("select balance from accounts where accid ="+accid);
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+ 
+		}
+		if(rs.next())
+		{
+			return rs.getDouble("balance");
+		}
+		else return 0;
+	}
+	
+	public double txDeposit() throws SQLException
+	{
+		ResultSet rs = null;
+		int accid =(int)(Math.random()*10000000+1) ;
+		int branchid = (int)(Math.random()*100+1);
+		int tellerid =(int)(Math.random()*1000+1) ;
+		double delta = (Math.random()*10000+1);
+		
+		
+		try {
+
+			stmnt.executeUpdate("update branches set balance ="+delta+" where branchid = "+branchid);
+			stmnt.executeUpdate("update tellers set balance = "+delta+" where tellerid = "+tellerid);
+			stmnt.executeUpdate("update accounts set balance = "+delta+" where accid = "+accid);
+			stmnt.executeUpdate("insert into history (accid, tellerid, delta, branchid, accbalance,cmmnt) VALUES("+accid+","+tellerid+","+delta+","+branchid+",(select balance from accounts where accid ="+accid+"),' ')");
+			rs = stmnt.executeQuery("select balance from accounts where accid = " + accid);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(rs.next())
+		{
+			return rs.getDouble("balance");
+		}
+		else return 0;
+	}
+	
+	public ResultSet txAnalysis() throws SQLException
+	{
+		ResultSet rs = null;
+		double delta = (Math.random()*10000+1);
+		try {
+
+			rs = stmnt.executeQuery("select * from history where delta ="+delta);
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public int getTxCount() {
+		return txCount;
+	}
+	
 }
